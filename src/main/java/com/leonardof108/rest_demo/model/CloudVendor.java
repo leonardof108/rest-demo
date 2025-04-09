4a51a3b65264d0319271271059af9eda6cc91dbb
Leonardo Ferreira <leonardof108@gmail.com>
2025-04-09 19:36:56 -0300

    changed h2 to postgres + refactoring with lombok

--- src/main/java/com/leonardof108/rest_demo/model/CloudVendor.java
+++ src/main/java/com/leonardof108/rest_demo/model/CloudVendor.java
@@ -1,56 +1,20 @@
 package com.leonardof108.rest_demo.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CloudVendor {
     @Id
     private String vendorId;
     private String vendorName;
     private String vendorAddress;
     private String vendorPhoneNumber;
-
-    public CloudVendor() {
-    }
-
-    public CloudVendor(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
-        this.vendorId = vendorId;
-        this.vendorName = vendorName;
-        this.vendorAddress = vendorAddress;
-        this.vendorPhoneNumber = vendorPhoneNumber;
-    }
-
-    public String getVendorId() {
-        return vendorId;
-    }
-
-    public void setVendorId(String vendorId) {
-        this.vendorId = vendorId;
-    }
-
-    public String getVendorName() {
-        return vendorName;
-    }
-
-    public void setVendorName(String vendorName) {
-        this.vendorName = vendorName;
-    }
-
-    public String getVendorAddress() {
-        return vendorAddress;
-    }
-
-    public void setVendorAddress(String vendorAddress) {
-        this.vendorAddress = vendorAddress;
-    }
-
-    public String getVendorPhoneNumber() {
-        return vendorPhoneNumber;
-    }
-
-    public void setVendorPhoneNumber(String vendorPhoneNumber) {
-        this.vendorPhoneNumber = vendorPhoneNumber;
-    }
 }
